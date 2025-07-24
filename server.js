a7419280166d2499f3842ae4dab17291d998f1e1
jiyaarora21 <arorajiya2101@gmail.com>
2025-07-25 02:10:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
+
 
 // ... previous+ code
 
@@ -22,11 +24,9 @@ app.get('/', (req, res) => {
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('✅ MongoDB connected'))
     .catch((err) => console.error('❌ MongoDB connection failed:', err));
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 const PORT = process.env.PORT || 5000;
-
-
-
 
 
 app.listen(PORT, () => {
